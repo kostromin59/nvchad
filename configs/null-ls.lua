befd0538ca35fb9f8776973d2a2a40313e23fa15
kostrominoff <zootikchannel@gmail.com>
2023-11-11 10:30:40 +0500

    Async format on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -58,7 +58,7 @@ null_ls.setup {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
+          vim.lsp.buf.format { bufnr = bufnr, async = true }
         end,
       })
     end
