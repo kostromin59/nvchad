3b695abf29f224cdfbe695bff9590bb6296b54ad
kostrominoff <zootikchannel@gmail.com>
2024-02-22 12:03:12 +0500

    Conform nvim

--- plugins.lua
+++ plugins.lua
@@ -7,17 +7,25 @@ local plugins = {
     "neovim/nvim-lspconfig",
     dependencies = {
       -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
+      -- {
+      --   "jose-elias-alvarez/null-ls.nvim",
+      --   config = function()
+      --     require "custom.configs.null-ls"
+      --   end,
+      -- },
     },
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end, -- Override to setup mason-lspconfig
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "custom.configs.conform"
+    end,
   },
 
   {
